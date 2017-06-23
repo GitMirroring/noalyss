cbb47f19e4a36731910bfaef78c46caa0c8181ec
Dany De Bontridder <danydb@noalyss.eu>
2017-06-24 01:37:25 +0200

    Typo

--- include/template/module.php
+++ include/template/module.php
@@ -9,7 +9,7 @@
     <div class="name">
 
 <?php
-$http=new HtmlInput();
+$http=new HttpInput();
 if ( $cn->get_value("select count(*) from profile join profile_user using (p_id)
 		where user_name=$1 and with_calc=true",array($_SESSION['g_user'])) ==1):
   echo '<div id="calc">';
