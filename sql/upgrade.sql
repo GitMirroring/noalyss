52849e775f915f47e3e19c1f003fa79fb6ed9cca
Dany De Bontridder <danydb@noalyss.eu>
2017-07-23 13:24:35 +0200

    Add new theme and  modify tmp_pcmn

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,1 +1,12 @@
 alter table action_gestion drop ag_ref_ag_id;
+--- repository insert into theme (the_name,the_filestyle) values ('Classic 692','style-r692.css');
+
+ALTER TABLE tmp_pcmn   ADD CONSTRAINT id_ux UNIQUE(id);
+ALTER TABLE tmp_pcmn ADD COLUMN id bigint;
+ALTER TABLE tmp_pcmn ALTER COLUMN id SET NOT NULL;
+ALTER TABLE tmp_pcmn ALTER COLUMN id SET DEFAULT nextval('tmp_pcmn_id_seq'::regclass);
+COMMENT ON COLUMN tmp_pcmn.id IS 'allow to identify the row, it is unique and not null (pseudo pk)';
+
+
+
+
