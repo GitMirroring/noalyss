4feba4e5ed229d9c1c1e43a3adb7b2372b3a31a6
Dany De Bontridder <dany@alchimerys.be>
2017-09-14 22:01:55 +0200

    task #0001463: Calculatrice - 4 décimales

--- html/js/calc.js
+++ html/js/calc.js
@@ -45,7 +45,7 @@ function cal()
 
         sub=eval(p_variable);
         var result=parseFloat(sub);
-        result=Math.round(result*100)/100;
+        result=Math.round(result*10000)/10000;
     }
     catch(exception)
     {
