17b8a0504bca7656fa9f3bee091d64331f292642
Dany De Bontridder <danydb@noalyss.eu>
2017-10-06 10:00:49 +0200

    HttpInput : do not use in_array but array_key_exists,

--- include/lib/http_input.class.php
+++ include/lib/http_input.class.php
@@ -86,7 +86,7 @@ class HttpInput
         {
             if (func_num_args()==3)
             {
-                if (in_array($p_name,$this->array) )
+                if (array_key_exists($p_name,$this->array) )
                 {
                     $this->check_type($p_name, $p_type);
                     return $this->array[$p_name];
@@ -96,7 +96,7 @@ class HttpInput
                     return $p_default;
                 }
             }
-            if (!in_array($p_name,$this->array))
+            if (!array_key_exists($p_name,$this->array))
             {
                 throw new Exception(_('Paramètre invalide')."[$p_name]",
                 EXC_PARAM_VALUE);
