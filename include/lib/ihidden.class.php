ba0e3a9be96a12da4c6fbd12f943fb7014edc531
Dany De Bontridder <danydb@noalyss.eu>
2017-12-05 12:05:28 +0100

    Fix ID problem

--- include/lib/ihidden.class.php
+++ include/lib/ihidden.class.php
@@ -26,17 +26,14 @@ require_once NOALYSS_INCLUDE.'/lib/html_input.class.php';
 class IHidden extends HtmlInput
 {
     /*!\brief show the html  input of the widget*/
-    public function input($p_name=null,$p_value=null)
+    public function input($p_name=null,$p_value=null,$id="")
     {
         $this->name=($p_name==null)?$this->name:$p_name;
         $this->value=($p_value==null)?$this->value:$p_value;
-		$this->id=($this->id=="")?$this->name:$this->id;
+        $this->id=($this->id=="")?$this->name:$this->id;
 
         $r='<INPUT TYPE="HIDDEN" id="'.$this->id.'" name="'.$this->name.'" value="'.$this->value.'">';
         return $r;
 
-    }
-    static public function test_me()
-    {
     }
 }
