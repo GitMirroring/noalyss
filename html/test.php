4af03a5fa2fd81cb2490ba19dea1939c9ee4c28b
Dany De Bontridder <danydb@noalyss.eu>
2018-01-20 12:16:54 +0100

    Task #0001529: Icon_Action missing

--- html/test.php
+++ html/test.php
@@ -34,6 +34,7 @@ include_once("lib/ac_common.php");
 require_once('lib/database.class.php');
 require_once ('class/dossier.class.php');
 require_once('lib/html_input.class.php');
+require_once('lib/icon_action.class.php');
 require_once ('lib/function_javascript.php');
 require_once 'class/user.class.php';
 require_once NOALYSS_INCLUDE.'/lib/http_input.class.php';
