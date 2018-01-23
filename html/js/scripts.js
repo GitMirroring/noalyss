576f5983a7d82ca506335d5564e81e8704e995d7
Dany De Bontridder <danydb@noalyss.eu>
2018-01-23 23:09:54 +0100

    esthetic

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -2875,7 +2875,7 @@ function create_anchor_up()
 function init_scroll()
 {
     var up=new Element('div',{"class":"inner_box",
-            "style":"padding:5px;left:auto;width:auto;height: auto;display:none;position:fixed;top:25px;right:50px;text-align:center",
+            "style":"padding:5px;left:auto;width:auto;height: auto;display:none;position:fixed;bottom:25px;right:50px;text-align:center",
             id:"go_up"
         });
         up.innerHTML=' <a class="icon" href="#up_top" >&#xe81a;</a><a href="javascript:show_calc()" class="icon">&#xf1ec;</a>';
@@ -2883,7 +2883,7 @@ function init_scroll()
          window.onscroll=function () {
          if ( document.viewport.getScrollOffsets().top> 0) {
              if ($('go_up').visible() == false) {
-                $('go_up').setOpacity(0.70); 
+                $('go_up').setOpacity(0.80); 
                 $('go_up').show();
                 $('go_up').style.zIndex=99;
             }
