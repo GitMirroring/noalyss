c33a3fed59a9ec803057c5adcaf5d8b169678bca
Dany De Bontridder <danydb@noalyss.eu>
2018-01-25 22:53:05 +0100

    Icon_Action replace image by fontello

--- include/lib/inplace_edit.class.php
+++ include/lib/inplace_edit.class.php
@@ -55,7 +55,7 @@ class Inplace_Edit
         $this->input=$p_input;
         $x["input"]=serialize($p_input);
         $this->json=json_encode($x, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_NUMERIC_CHECK);
-        $this->message=_("Faites un choix");
+        $this->message=_("Cliquez pour éditer");
     }
     ///@brief build a Inplace_Edit object from
     /// a serialized string (ajax json parameter = input)
@@ -111,7 +111,7 @@ EOF;
         $v=$this->input->get_value();
         $v=(trim($v)=="")?$this->message:$v;
         echo $v,
-                 "<img src=\"image/button-edit.png\"/>",
+                 '<span class="smallicon icon">&#xe80d;</span> ',
                 "
             <script>
             $('{$this->input->id}edit').removeClassName('inplace_edit_input');
@@ -133,7 +133,7 @@ EOF;
         $v=$this->input->get_value();
         $v=(trim($v)=="")?$this->message:$v;
         echo $v;
-        echo "<img src=\"image/button-edit.png\"/>";
+        echo'<span class="smallicon icon">&#xe80d;</span> ';
         echo "</span>";
         echo "
             
