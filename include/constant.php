0a5fd5c1ff8e074491d2c9f3f2d04b4d75bab472
Dany De Bontridder <danydb@noalyss.eu>
2018-01-27 10:50:04 +0100

    correct bug created by the  merge

--- include/constant.php
+++ include/constant.php
@@ -309,8 +309,5 @@ define ('EMAIL_LIMIT',1002);
 define ('EXC_PARAM_VALUE',1005);
 define ('EXC_PARAM_TYPE',1006);
 define ('EXC_DUPLICATE',1200);
-define ('ICONON','icon-on.png');
-define ('ICONOFF','icon-off.png');
 define ("PINDG","&#xf047;");
 define ("UNPINDG","&#xf192;");
-//define ("UNPINDG","&#8778;");
