118bc4907602ef2759c220bb633b3a15a36c4fda
Dany De Bontridder <danydb@noalyss.eu>
2018-02-05 20:47:04 +0100

    Missing lib

--- include/lib/html_input.class.php
+++ include/lib/html_input.class.php
@@ -18,6 +18,7 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 require_once NOALYSS_INCLUDE.'/lib/http_input.class.php';
+require_once NOALYSS_INCLUDE.'/lib/icon_action.class.php';
 
 // Copyright Author Dany De Bontridder danydb@aevalys.eu
 
