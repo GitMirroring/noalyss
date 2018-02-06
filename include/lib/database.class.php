84c05e8154cf278379eb9e8efca521c4fb227d5e
Dany De Bontridder <danydb@noalyss.eu>
2018-02-06 21:29:21 +0100

    Small bug : new structure for table version

--- include/lib/database.class.php
+++ include/lib/database.class.php
@@ -378,7 +378,7 @@ class Database
 
     function get_version()
     {
-        $Res=$this->get_value("select val from version");
+        $Res=$this->get_value("select max(val) from version");
         return $Res;
     }
 
