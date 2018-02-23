2a84e48529a256f0ef509867e570cde6c2855b94
Dany De Bontridder <danydb@noalyss.eu>
2018-02-23 20:18:43 +0100

    Icon_Action::trash remove unwanted char

--- include/lib/icon_action.class.php
+++ include/lib/icon_action.class.php
@@ -192,7 +192,7 @@ class Icon_Action
      */
     static function trash($p_id,$p_javascript) 
     {
-        $r='<span id="'.$p_id.'" onclick="'.$p_javascript.'" class="icon">&#xe80f;;</span>';
+        $r='<span id="'.$p_id.'" onclick="'.$p_javascript.'" class="icon">&#xe80f;</span>';
         return $r;
     }
 }
