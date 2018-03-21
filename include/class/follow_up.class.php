86d8e45aab50715deae8c55b85f9d93202bd8a40
Dany De Bontridder <dany@alchimerys.be>
2018-03-21 21:39:15 +0100

    Follow-up missing file from action-detail.php

--- include/class/follow_up.class.php
+++ include/class/follow_up.class.php
@@ -38,6 +38,7 @@ require_once NOALYSS_INCLUDE.'/lib/sort_table.class.php';
 require_once NOALYSS_INCLUDE.'/lib/irelated_action.class.php';
 require_once NOALYSS_INCLUDE.'/class/tag.class.php';
 require_once NOALYSS_INCLUDE.'/class/default_menu.class.php';
+require_once NOALYSS_INCLUDE.'/lib/inplace_edit.class.php';
 /**
  * \file
  * \brief class_action for manipulating actions
