71962e728bcae12d1c53a2474e73217bc9e7a7f6
Dany De Bontridder <danydb@noalyss.eu>
2018-04-19 10:23:19 +0200

    create currency SQL tables

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,9 +1,26 @@
-create view v_tva_rate as select 
-	tva_id,
-	tva_rate,
-	tva_label,
-	tva_comment,
-	split_part(tva_poste,',',1) as tva_purchase,
-	split_part(tva_poste,',',2) as tva_sale,
-	tva_both_side 
-from tva_rate;
+-- Drop table
+
+-- DROP TABLE public.currency
+
+CREATE TABLE public.currency (
+	id serial NOT NULL,
+	cr_code_iso varchar(10) NULL,
+	CONSTRAINT currency_pk PRIMARY KEY (id),
+	CONSTRAINT currency_un UNIQUE (cr_code_iso)
+);
+
+-- Drop table
+
+-- DROP TABLE public.currency_history
+
+CREATE TABLE public.currency_history (
+	id serial NOT NULL,
+	ch_value numeric(6) NOT NULL,
+	ch_from timestamp NOT NULL,
+	currency_id int4 NOT NULL,
+	CONSTRAINT currency_history_pk PRIMARY KEY (id),
+	CONSTRAINT currency_history_currency_fk FOREIGN KEY (id) REFERENCES currency(id)
+)
+;
+
+-- Ajouter commentaire sur colonne
