b9b28694d22ebad9cb7743cf135d1df67a0fdfad
Dany De Bontridder <danydb@noalyss.eu>
2018-09-15 23:17:23 +0200

    Bug #1 CFGTVA : impossible d'effacer un taux de TVA

--- include/class/tva_rate_mtable.class.php
+++ include/class/tva_rate_mtable.class.php
@@ -272,7 +272,7 @@ class Tva_Rate_MTable extends Manage_Table_SQL
         if ( $count < 2) {
             throw new Exception(_("Vous ne pouvez pas effacer tous les taux. Si votre société n'utilise pas la TVA, changer dans le menu société"));
         }
-
+        $cn->exec_sql("delete from tva_rate where tva_id=$1", [$this->table->tva_id]);
     }
 
 }
