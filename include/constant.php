6a40e87d49bf2f128044b1af351f8a7f8e8c0c56
Dany De Bontridder <danydb@noalyss.eu>
2018-11-07 13:00:06 +0100

    constant.php , clean ini_set

--- include/constant.php
+++ include/constant.php
@@ -62,11 +62,10 @@ if ( strpos($inc_path,";") != 0 ) {
   $os=1;			/* $os is 1 for unix */
 }
 set_include_path($new_path);
-ini_set ('default_charset',"UTF-8");
-ini_set ('session.use_cookies',1);
-ini_set ('magic_quotes_gpc','off');
-ini_set ('session.use_only_cookies','on');
-ini_set ('session.use_cookies',1);
+@ini_set ('default_charset',"UTF-8");
+@ini_set ('session.use_cookies',1);
+@ini_set ('magic_quotes_gpc','off');
+@ini_set ('session.use_only_cookies','on');
 
 if ( ! defined('OVERRIDE_PARAM')) {
     ini_set ('max_execution_time',240);
