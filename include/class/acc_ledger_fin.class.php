17ffe67ddcbe6db6a2038a15b286512c24066f8b
Dany De Bontridder <danydb@noalyss.eu>
2018-11-27 07:46:34 +0100

    translate

--- include/class/acc_ledger_fin.class.php
+++ include/class/acc_ledger_fin.class.php
@@ -524,7 +524,7 @@ class Acc_Ledger_Fin extends Acc_Ledger
 
 		$r.='</fieldset>';
 
-		$r.='<div class="myfieldset"><h1 class="legend">Extrait de compte</h1>';
+		$r.='<div class="myfieldset"><h1 class="legend">'._("Extrait de compte").'</h1>';
 		//--------------------------------------------------
 		// Saldo begin end
 		//-------------------------------------------------
@@ -532,10 +532,10 @@ class Acc_Ledger_Fin extends Acc_Ledger
 		$r.='<tr>';
 		// Extrait
 		//--
-		$r.=tr('<td> Numéro d\'extrait</td>' . td(h($e_pj)));
-		$r.='<tr><td >Solde début extrait </td>';
+		$r.=tr('<td>'._("Numéro d'extrait").' </td>' . td(h($e_pj)));
+		$r.='<tr><td >'._("Solde début extrait").' </td>';
 		$r.='<td style="num">' . nbm($first_sold) . '</td></tr>';
-		$r.='<tr><td>Solde fin extrait </td>';
+		$r.='<tr><td>'._("Solde fin extrait").' </td>';
 		$r.='<td style="num">' . nbm($last_sold) . '</td></tr>';
 		$r.='</table>';
 
@@ -545,11 +545,11 @@ class Acc_Ledger_Fin extends Acc_Ledger
 		//-------------------------------------------------
 		$r.='<TABLE style="width:100%" id="fin_item">';
 		$r.="<TR>";
-		if ($chdate==2) $r.='<th>Date</th>';
-		$r.="<th style=\"width:auto;text-align:left\" colspan=\"2\">Nom</TH>";
-		$r.="<th style=\"text-align:left\" >Commentaire</TH>";
-		$r.="<th style=\"text-align:right\">Montant</TH>";
-		$r.='<th colspan="2"> Op. Concern&eacute;e(s)</th>';
+		if ($chdate==2) $r.='<th>'._("Date").'</th>';
+		$r.="<th style=\"width:auto;text-align:left\" colspan=\"2\">"._("Nom")."</th>";
+		$r.="<th style=\"text-align:left\" >"._("Commentaire")."</th>";
+		$r.="<th style=\"text-align:right\">"._("Montant")."</th>";
+		$r.='<th colspan="2">'._("Op. Concernée(s)").'</th>';
 
 		/* if we use the AC */
 		if ($g_parameter->MY_ANALYTIC != 'nu')
@@ -631,14 +631,14 @@ class Acc_Ledger_Fin extends Acc_Ledger
 		$r.="</TABLE>";
 
 		// saldo
-		$r.='<br>Ancien solde = ' . $solde;
+		$r.='<br>'.sprintf(_("Ancien solde = %d"), $solde);
 		$new_solde+=$tot_amount;
-		$r.='<br>Nouveau solde = ' . $new_solde;
-		$r.='<br>Difference =' . $tot_amount;
+		$r.='<br>'.sprintf(_("Nouveau solde = %d"),$new_solde);
+		$r.='<br>'.sprintf(_("Difference = %d"), $tot_amount);
 		// check for upload piece
 		$file = new IFile();
 
-		$r.="<br>Ajoutez une pi&egrave;ce justificative ";
+		$r.="<br>"._("Ajoutez une pièce justificative")." ";
 		$r.=$file->input("pj", "");
 
 		$r.='</div>';
