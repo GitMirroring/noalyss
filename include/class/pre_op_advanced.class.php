7faeb862c1b3a1639672dde835769775105d0252
Dany De Bontridder <danydb@noalyss.eu>
2018-12-17 18:04:44 +0100

    predefined operation , invalid value for database::setp

--- include/class/pre_op_advanced.class.php
+++ include/class/pre_op_advanced.class.php
@@ -248,7 +248,7 @@ class Pre_Op_Advanced extends Pre_operation_detail
                 if ($poste->value != '')
                 {
                         $Poste = new Acc_Account($legder->db);
-                        $Poste->set_parameter('value', $poste->value);
+                        $Poste->set_parameter('pcm_val', $poste->value);
                         $label = $Poste->get_lib();
                 }
 
