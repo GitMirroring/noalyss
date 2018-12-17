3efb8904feb19c3345610af4c1d0299d792cb188
Dany De Bontridder <danydb@noalyss.eu>
2018-12-17 23:30:55 +0100

    cfgcurrency : file for the new version : if the file to include does not exist then display a warning and exit

--- include/lib/ac_common.php
+++ include/lib/ac_common.php
@@ -1103,10 +1103,16 @@ function show_menu($module)
                         }
 			// if file is not a plugin, include the file, otherwise
 			// include the plugin launcher
-			if ( $file[0]['me_type'] != 'PL')
+			if ( $file[0]['me_type'] != 'PL') {
+                            if (file_exists($file[0]['me_file']))
+                            {
 				require_once $file[0]['me_file'];
-			else
+                            } else {
+                                echo echo_warning(_("Fichier non trouvé"));
+                            }
+                        } else {
 				require 'extension_get.inc.php';
+                        }
 
 			exit();
 		}
