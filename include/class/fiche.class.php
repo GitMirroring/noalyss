27f5673f2d8af5b411ee191b8b02308596c1c00e
Dany De Bontridder <danydb@noalyss.eu>
2019-07-22 21:22:16 +0200

    Bug : card cannot use a attribute of the type "card", reason : the query was wrong and there were a confusion between this->name and this->id in "get_js_attr()"

--- include/class/fiche.class.php
+++ include/class/fiche.class.php
@@ -654,7 +654,7 @@ class Fiche
                             $w->extra=$filter;
                             $w->extra2=0;
                             $label=new ISpan();
-                            $label->name="av_text".$uniq.$r->ad_id."_label";
+                            $label->name="av_text".$r->ad_id.$uniq."_label";
                             $fiche=new Fiche($this->cn);
                             $fiche->get_by_qcode($r->av_text);
                             if ($fiche->id==0)
@@ -670,7 +670,7 @@ class Fiche
                             }
                             $w->set_attribute('ipopup', 'ipopcard');
                             $w->set_attribute('typecard', $filter);
-                            $w->set_attribute('inp', "av_text".$r->ad_id);
+                            $w->set_attribute('inp', $w->id);
                             $w->set_attribute('label', $label->name);
                             $w->autocomplete=0;
                             $w->dblclick="fill_ipopcard(this);";
