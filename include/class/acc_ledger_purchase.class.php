3c3d4c3b4b5064b6bd36cfae511c1780528fda09
Dany De Bontridder <danydb@noalyss.eu>
2019-10-25 11:37:50 +0200

    Save_form_plan must use the array

--- include/class/acc_ledger_purchase.class.php
+++ include/class/acc_ledger_purchase.class.php
@@ -665,7 +665,7 @@ class  Acc_Ledger_Purchase extends Acc_Ledger
                     $op->oa_date=$e_date;
                     $op->oa_debit='t';
                     $op->oa_description=sql_string($e_comm);
-                    $op->save_form_plan($_POST,$i,$j_id);
+                    $op->save_form_plan($p_array,$i,$j_id);
                 }
                 // insert into quant_purchase
                 //-----
