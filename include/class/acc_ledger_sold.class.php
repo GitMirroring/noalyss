9d1f3f0821f7feb0d022bdb7595d39474bd2af31
Dany De Bontridder <danydb@noalyss.eu>
2019-11-20 20:56:28 +0100

    Bug : when e_march is null it is considered as an empty string instead of 0

--- include/class/acc_ledger_sold.class.php
+++ include/class/acc_ledger_sold.class.php
@@ -374,6 +374,9 @@ class Acc_Ledger_Sold extends Acc_Ledger {
                     $op->oa_description = sql_string($e_comm);
                     $op->save_form_plan($_POST, $i, $j_id);
                 }
+                if (empty( ${'e_march' . $i . '_price'}  ) ) ${'e_march' . $i . '_price'}  = 0;
+                if (empty( ${'e_march' . $i }  ) ) ${'e_march' . $i }  = 0;
+                if (empty( ${'e_quant' . $i }  ) ) ${'e_quant' . $i }  = 0;
                 if ($g_parameter->MY_TVA_USE == 'Y') {
                     /* save into quant_sold */
                     $r = $this->db->exec_sql("select insert_quant_sold ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)", array(null, /* 1 */
