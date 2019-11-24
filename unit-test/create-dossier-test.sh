9f54258d8f048a073de6ba30f609612bddec3290
Dany De Bontridder <danydb@noalyss.eu>
2019-11-24 21:59:47 +0100

    File to restore for testing

--- unit-test/create-dossier-test.sh
+++ unit-test/create-dossier-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DOSSIER_TEST=rel70dossier25
+FILE_TEST=dossiertest191124-2109.bin
 
 dropdb $DOSSIER_TEST
 createdb $DOSSIER_TEST
-pg_restore -Fc --no-owner --no-privilege --verbose  -d $DOSSIER_TEST  db/dossiertest191117-0909.bin
+pg_restore -Fc --no-owner --no-privilege --verbose  -d $DOSSIER_TEST  db/$FILE_TEST
 
