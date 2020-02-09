b9fd0bbe8786f06afb9e06d399b252be363f8d00
Dany De Bontridder <danydb@noalyss.eu>
2020-02-09 13:14:36 +0100

    phpunit  : use plain text for restoring test database

--- unit-test/create-dossier-test.sh
+++ unit-test/create-dossier-test.sh
@@ -5,5 +5,5 @@ FILE_TEST=dossiertest191124-2109.bin
 
 dropdb $DOSSIER_TEST
 createdb $DOSSIER_TEST
-pg_restore -Fc --no-owner --no-privilege --verbose  -d $DOSSIER_TEST  db/$FILE_TEST
+pg_restore -Fp --no-owner --no-privilege --verbose  -d $DOSSIER_TEST  db/$FILE_TEST
 
