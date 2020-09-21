4628b10f2a3f3e5f044049bdc27629bd35bcdfff
Dany De Bontridder <danydb@noalyss.eu>
2020-09-21 22:11:14 +0200

    Bug 1834 : order POST over GET

--- include/class/acc_operation.class.php
+++ include/class/acc_operation.class.php
@@ -747,14 +747,9 @@ class Acc_Operation
             $r.=_("Menu invalide");
             return $r;
         }
-        /** 
-         * @bug : if several menu possible , will use only the first one, because request take first the GET and after
-         * the POST
-         */
+       
         // Prepare the form
-        $r=sprintf('<form id="%s" method="POST" ACTION="%s">',$p_id,NOALYSS_URL."/do.php?".http_build_query([
-                "ac"=>$a_code[0]['code'],"gDossier"=>Dossier::id()
-        ]));
+        $r=sprintf('<form id="%s" method="POST" ACTION="%s">',$p_id,NOALYSS_URL."/do.php?".Dossier::get());
         $r.=Dossier::hidden();
         // select the menu where the operation will be duplicated
         $r.="<p>";
