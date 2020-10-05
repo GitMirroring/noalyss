4bab05f76b53238d466b40dda3d75f9d2dc20421
Dany De Bontridder <danydb@noalyss.eu>
2020-10-05 13:50:06 +0200

    Action and Document Add missing files

---
+++ include/ajax/ajax_cfgaction.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ *   This file is part of NOALYSS.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+// Copyright (2002-2020) Author Dany De Bontridder <danydb@noalyss.eu>
+
+if (!defined('ALLOWED'))
+    die('Appel direct ne sont pas permis');
+
+// check right
+if ($g_user->check_action(PARCATDOC)==0)
+{
+
+    record_log("cfgaction01 security ");
+    return;
+}
+
+require_once NOALYSS_INCLUDE."/class/action_document_type_mtable.class.php";
+
+$http=new HttpInput();
+
+
+/**
+ * @file
+ * @brief call for document_type in follow up
+ */
+try
+{
+    $table=$http->request('table');
+    $ctl_id=$http->request('ctl');
+    $action=$http->request('action');
+    $p_id=$http->request('p_id', "number");
+}
+catch (Exception $ex)
+{
+    echo $e->getMessage();
+    return;
+}
+
+$doc_type=new Document_type_SQL($cn,$p_id);
+$action_document_type=new Action_Document_Type_MTable($doc_type);
+
+$action_document_type->set_callback("ajax_misc.php");
+$action_document_type->add_json_param("op", "cfgaction");
+$action_document_type->set_object_name($ctl_id);
+
+
+
+if ($action=="input")
+{
+   
+    header('Content-type: text/xml; charset=UTF-8');
+    echo $action_document_type->ajax_input()->saveXML();
+    return;
+}
+elseif ($action=="save")
+{
+    $xml=$action_document_type->ajax_save();
+    header('Content-type: text/xml; charset=UTF-8');
+    echo $xml->saveXML();
+}
+elseif ($action=="delete")
+{
+    $xml=$action_document_type->ajax_delete();
+    header('Content-type: text/xml; charset=UTF-8');
+    echo $xml->saveXML();
+}
---
+++ include/template/action_document_type_mtable_input.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ *   This file is part of NOALYSS.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+// Copyright (2002-2019) Author Dany De Bontridder <danydb@noalyss.eu>
+
+if (!defined('ALLOWED'))
+    die('Appel direct ne sont pas permis');
+
+/**
+ * @file
+ * @brief called from p_id already set, 
+ * @see action_document_type_mtable::input
+ */
+// SQL Object Document_Type
+$table=$this->get_table();
+// db connx
+$cn=$table->cn;
+?>
+
+<?php echo _('Prochain numéro') ?>
+<p class="info" style="display:inline">
+    (
+<?php echo _('numéro actuel') ?>
+<?php
+$last=0;
+if ( $table->dt_id > 0) {
+    $ret=$cn->get_array("select last_value,is_called from seq_doc_type_".$table->dt_id);
+
+    $last=$ret[0]['last_value'];
+    /* !
+     * \note  With PSQL sequence , the last_value column is 1 when before   AND after the first call, to make the difference between them
+     * I have to check whether the sequence has been already called or not */
+    if ($ret[0]['is_called']=='f')
+        $last--;
+}
+echo $last;
+?>
+    )
+</p>
+
+<?php
+echo
+Icon_Action::infobulle(15);
+?>
+<?php
+$seq=new INum('seq', 0);
+echo $seq->input();
+?>
+<div>
+    <h3 class="info" sytle="margin-block: 4px"><?php echo _("Détail") ?></h3>
+    <ul class="tab_row" style="padding-top: 0px">
+        <li>
+<?php
+$i=new ICheckBox("detail_operation",1);
+if ( Document_Option::is_enable_operation_detail($table->dt_id)) $i->set_check(1);else $i->set_check(0);
+echo $i->input();
+echo _("Détail opération");
+?>
+        </li> 
+        <li>
+<?php
+$i=new ICheckBox("det_contact_mul",1);
+if ( Document_Option::is_enable_contact_multiple($table->dt_id)) $i->set_check(1); else $i->set_check(0);
+echo $i->input();
+echo _("Contacts multiples");
+?>
+        </li> 
+        <li>
+<?php
+$i=new ICheckBox("make_invoice",1);
+if ( Document_Option::is_enable_make_invoice($table->dt_id)) $i->set_check(1); else $i->set_check(0);
+echo $i->input();
+echo _("Création de facture");
+?>
+        </li> 
+    </ul>
+
+</div>
