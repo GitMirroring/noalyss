91874630103903800211eddfa626b2b6e8f423cd
sparkyx <danydb@noalyss.eu>
2020-10-30 10:19:17 +0100

    Correct SQL script

--- include/sql/patch/upgrade148.sql
+++ include/sql/patch/upgrade148.sql
@@ -1,5 +1,5 @@
 begin;
-INSERT INTO public.document_option (do_code, document_type_id, do_enable, do_option) select 'followup_comment', dt_id, 1, NULL
+INSERT INTO public.document_option (do_code, document_type_id, do_enable, do_option) select 'followup_comment', dt_id, 1, 'SOME_FIXED'
 from document_type ;
 
 insert into version (val,v_description) values (149,'Default values for document_option,comment on followup');
