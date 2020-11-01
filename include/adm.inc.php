86aedf7452494bc4c666cd102b8eadffd7f4a4dc
Dany wm De Bontridder <danydb@noalyss.eu>
2020-11-01 20:25:42 +0100

    Cosmetic : add waiting box when searching by card category for Management customer, supplier,...

--- include/adm.inc.php
+++ include/adm.inc.php
@@ -86,7 +86,7 @@ if ( $low_action == "list" )
                                              ' where  frd_id=$1 '.
                                              ' order by fd_label ',1,array(FICHE_TYPE_ADM_TAX));
             $sel_card->selected=$http->get("cat","number",-1);
-            $sel_card->javascript=' onchange="submit(this);"';
+            $sel_card->javascript=' onchange="waiting_box();submit(this);"';
             echo _('Catégorie :').$sel_card->input();
         } else
         {
