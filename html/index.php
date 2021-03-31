c71a35f4374241177e2dc6888bcb615af9465167
sparkyx <danydb@noalyss.eu>
2021-03-31 11:37:15 +0200

    Set version 9.0

--- html/index.php
+++ html/index.php
@@ -214,13 +214,13 @@ if (isset($_REQUEST['reconnect'])&&isset($_REQUEST['backurl']))
 ?>
 <div >
     <div class="d-sm-block">
-        <IMG SRC="image/logo8100.png" id="logo_id" alt="NOALYSS">
+        <IMG SRC="image/logo9000.png" id="logo_id" alt="NOALYSS">
 
     </div>    
     <div class="container">
 
         <div class="mx-auto" id="login_div">
-        <h1 style="text-align: center;color:darkblue">NOALYSS</h1>
+        <h1 style="text-align: center;color:darkblue;font-weight: 800">NOALYSS</h1>
             <form id="login_frm" action="login.php" method="post" name="loginform" class="p-sm-3" >
                 <?php echo $goto; ?>
 
