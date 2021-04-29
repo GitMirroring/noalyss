1dc7fbc206126f13b7e54112ec02411db85dd71a
sparkyx <danydb@noalyss.eu>
2021-04-29 14:52:28 +0200

    Cosmetic : width to large if recover passwd enable and icon trash not available for document (in operation detail)

--- include/lib/ac_common.php
+++ include/lib/ac_common.php
@@ -432,6 +432,7 @@ function html_min_page_start($p_theme="", $p_script="", $p_script2="")
     <script src=\"js/scripts.js\" type=\"text/javascript\"></script>
     <script src=\"js/acc_ledger.js\" type=\"text/javascript\"></script>
     <script src=\"js/smoke.js\" type=\"text/javascript\"></script>";
+    echo "<LINK id=\"pagestyle\" REL=\"stylesheet\" type=\"text/css\" href=\"css/font/fontello/css/fontello.css\" media=\"screen\"/>";
     include_once NOALYSS_INCLUDE.'/lib/message_javascript.php';
     echo '</HEAD>
     ';
