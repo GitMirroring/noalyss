d8479483bdfafaa6f35e0e1097bcc59c4938bfe8
sparkyx <danydb@noalyss.eu>
2021-05-04 19:27:57 +0200

    PLpgSQL function , format properly the accounting before inserting fiche_detail and tmp_pcmn tables

--- include/constant.php
+++ include/constant.php
@@ -116,7 +116,7 @@ if ( !defined ("NOALYSS_PACKAGE_REPOSITORY")) {
 if ( ! defined ("SYSINFO_DISPLAY")) {
     define ("SYSINFO_DISPLAY",TRUE);
 }
-define ("DBVERSION",158);
+define ("DBVERSION",159);
 define ("MONO_DATABASE",25);
 define ("DBVERSIONREPO",19);
 define ('NOTFOUND','--not found--');
