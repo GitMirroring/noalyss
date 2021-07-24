8fbc065345529029c97b3e5c4ad2ac78196f22a2
Dany wm De Bontridder <danydb@noalyss.eu>
2021-07-24 20:16:51 +0200

    Cosmetic #0002047: Historique : montant total n'est pas dans la bonne colonne

--- include/class/acc_ledger_search.class.php
+++ include/class/acc_ledger_search.class.php
@@ -1001,7 +1001,7 @@ class Acc_Ledger_Search
         $tot=round($tot, 4);
         $r.="<TR>";
         $r.='<TD COLSPAN="5">Total</TD>';
-        $r.=td("").td("").td("").td('');
+        $r.=td("").td("");
         $r.='<TD ALIGN="RIGHT">'.nbm($tot)."</TD>";
         $r.="</tr>";
         if ($p_paid!=0)
