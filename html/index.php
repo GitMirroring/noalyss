7bc7bed8d831ed732c0e29e70d67b30aa99ff258
sparkyx <danydb@noalyss.eu>
2021-12-02 17:25:11 +0100

    Fix doc

--- html/index.php
+++ html/index.php
@@ -17,10 +17,12 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* ! \file
+/*! 
+ * \file
  * \brief default page where user access
  */
-/* ! \mainpage NOALYSS
+/*!
+ * \mainpage NOALYSS
  * Documentation
  * - \subpage Francais
  * - \subpage English
