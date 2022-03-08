032bd0f338fbc37cff5072972ef71bb123e9f466
sparkyx <danydb@noalyss.eu>
2022-03-08 09:21:09 +0100

    Fixed : error when column contains only a input element (checkbox)

--- html/js/sorttable.js
+++ html/js/sorttable.js
@@ -277,8 +277,8 @@ var sorttable = {
 
         hasInputs = (typeof node.getElementsByTagName == 'function') &&
                 node.getElementsByTagName('input').length;
-
-        if (node.getAttribute("sorttable_customkey") != null) {
+        
+        if (node.getAttribute && node.getAttribute("sorttable_customkey") != null) {
             return node.getAttribute("sorttable_customkey");
         } else if (typeof node.textContent != 'undefined' && !hasInputs) {
             return node.textContent.replace(/^\s+|\s+$/g, '');
