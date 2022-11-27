c4540d17db4e7c2462c120403b3a9bcce5c96bff
sparkyx <danydb@noalyss.eu>
2022-11-27 19:20:04 +0100

    fix bug followup cannot add first comment

--- include/class/follow_up.class.php
+++ include/class/follow_up.class.php
@@ -608,12 +608,12 @@ class Follow_Up
         $doc->upload($this->ag_id);
         if (trim($this->ag_comment)!='' && Document_Option::can_add_comment($this->ag_id))
         {
-            $this->db->exec_sql("insert into action_gestion_comment (ag_id,tech_user,agc_comment,agc_comment_raw) values ($1,$2,$3)"
+            $this->db->exec_sql("insert into action_gestion_comment (ag_id,tech_user,agc_comment,agc_comment_raw) values ($1,$2,$3,$4)"
                 , array($this->ag_id, $_SESSION[SESSION_KEY.'g_user'], strip_tags($this->ag_description),$this->ag_comment));
         }
         if (trim($this->ag_description)!='' && Document_Option::can_add_comment($this->ag_id))
         {
-            $this->db->exec_sql("insert into action_gestion_comment (ag_id,tech_user,agc_comment,agc_comment_raw) values ($1,$2,$3)"
+            $this->db->exec_sql("insert into action_gestion_comment (ag_id,tech_user,agc_comment,agc_comment_raw) values ($1,$2,$3,$4)"
                 , array($this->ag_id, $_SESSION[SESSION_KEY.'g_user'],strip_tags($this->ag_description), $this->ag_description));
         }
         $this->insert_operation();
