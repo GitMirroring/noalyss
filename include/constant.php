d2bbda5bc8400495f525addc90f614c1551c6293
sparkyx <danydb@noalyss.eu>
2022-11-27 19:21:01 +0100

    Change DB version

--- include/constant.php
+++ include/constant.php
@@ -116,7 +116,7 @@ if (!defined("NOALYSS_PACKAGE_REPOSITORY")) {
 if (!defined("SYSINFO_DISPLAY")) {
     define("SYSINFO_DISPLAY", TRUE);
 }
-define("DBVERSION", 181);
+define("DBVERSION", 182);
 define("MONO_DATABASE", 25);
 define("DBVERSIONREPO", 20);
 define('NOTFOUND', '--not found--');
