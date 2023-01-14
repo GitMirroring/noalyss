7eb0a5a72c7007b6917f9884ef508d0ef754bf8a
sparkyx <danydb@noalyss.eu>
2023-01-14 14:07:34 +0100

    Improve security : replace $_POST by Http_Input

--- include/dossier.inc.php
+++ include/dossier.inc.php
@@ -82,7 +82,7 @@ if ( isset ($_POST["DATABASE"]) )
         {
             alert(_('Désolé vous devez migrer ce modèle en unicode'));
             echo '<span class="error">';
-            echo _('le modele ').domaine.'mod'.$_POST["FMOD_ID"]._(" doit être migré en unicode.");
+            echo _('le modele ').domaine.'mod'.$http->post("FMOD_ID","number")._(" doit être migré en unicode.");
             echo _('Pour le passer en unicode, faites-en une sauvegarde puis restaurez le fichier reçu').'</span>';
             echo HtmlInput::button_anchor('Retour','admin-noalyss.php?action=dossier_mgt');
             return;
