ff70feba5b893b90d42eec6e4d07de2b3d06e04c
sparkyx <danydb@noalyss.eu>
2023-04-14 13:43:00 +0200

    Code Documentation

--- include/class/calendar.class.php
+++ include/class/calendar.class.php
@@ -32,6 +32,16 @@ class Calendar
 {
     var $current_date;
     private static $nb_day=array(31,28,31,30,31,30,31,31,30,31,30,31);
+    var    $month;         // current month
+    var    $day  ;         // day
+    var    $year;          // year
+    var    $action_div;    // array of action
+
+    // following are completed by fill_from_action
+    var    $action;        // array of event - follow-up (T: action_gestion)
+    var    $str_name;      // array of tiers for event - follow-up (T: action_gestion)
+    var    $title;         // array of title for each event - follow-up (T: action_gestion)
+    var    $hour ;         // array of hour for each event - follow-up (T: action_gestion)
 
     function __construct()
     {
