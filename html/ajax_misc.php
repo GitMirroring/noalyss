121abf8bf1f20753fa5c0408fcd3daafd83b5025
sparkyx <danydb@noalyss.eu>
2023-07-30 17:59:32 +0200

    replace  by NOALYSS_INCLUDE

--- html/ajax_misc.php
+++ html/ajax_misc.php
@@ -38,8 +38,8 @@
 if ( ! defined('ALLOWED')) define ('ALLOWED',1);
 
 require_once '../include/constant.php';
-global $dirname;
-require_once $dirname."/lib/ac_common.php";
+
+require_once NOALYSS_INCLUDE."/lib/ac_common.php";
 
 global $http;
 $http=new HttpInput();
