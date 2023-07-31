2a1a971636f58d51a3ccfad746b341cd94c322c3
sparkyx <danydb@noalyss.eu>
2023-07-31 12:00:11 +0200

    Fix document generate

--- include/class/document.class.php
+++ include/class/document.class.php
@@ -1800,6 +1800,7 @@ class Document
     static function replace_value($p_buffer, $p_pattern, $p_value, $p_limit=-1, $p_type='OOo')
     {
         $check=$p_pattern;
+        $p_value=$p_value??'';
         $check=str_replace(['&lt;', '&gt;', '<', '>', '='], "", $check);
         if (preg_replace('/[^[:alnum:]^_]/', '', $check)!=$check)
         {
@@ -1822,7 +1823,7 @@ class Document
         }
         if ($count==0)
         {
-	    $p_value='';
+
             if ($p_type=='OOo')
             {
                 $p_value=noalyss_str_replace('&', '&amp;', $p_value);
