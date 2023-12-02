4f1d7e155a56280d098b863e5267823ac5d465e0
sparkyx <danydb@noalyss.eu>
2023-12-02 18:20:27 +0100

    Improve documentation

--- include/lib/manage_table_sql.class.php
+++ include/lib/manage_table_sql.class.php
@@ -75,6 +75,26 @@
 
  * @endcode
  *
+ * The afterSaveFct is the function called after saving, the param is the HTML Element
+ @code PHP
+// to redirect : we take the pk_id and redirect to another location
+
+      $obj=$this->get_object_name();
+      $url=DRIVINGSCHOOL_URL;
+      $script=<<<EOF
+(function(){
+{$obj}.afterSaveFct=function(p_param) {
+   let student=p_param.attributes["ctl_pk_id"].value;
+   window.location="{$url}/do.php?do=student&student_id="+student+"&act=detail";
+ }})();
+EOF;
+
+echo create_script($script);
+ {$obj}.afterSaveFct=function(p_param) {
+   let student=p_param.attributes["ctl_pk_id"].value;
+   window.location="{$url}/do.php?do=student&student_id="+student+"&act=detail";
+ }})();
+@endcode
  */
 
 class Manage_Table_SQL
