b1f96914d81fbd2d26f4c4a92a42d7f8253d408d
sparkyx <danydb@noalyss.eu>
2024-02-03 16:13:09 +0100

    bug follow-up

--- include/class/follow_up.class.php
+++ include/class/follow_up.class.php
@@ -995,8 +995,12 @@ class Follow_Up
                                 where agc_id = $4 "
                     , array(strip_tags($this->ag_description), $this->ag_description, $_SESSION[SESSION_KEY.'g_user'],
                         $this->ag_description_id));
-            $this->db->exec_sql("insert into action_comment_document(document_id,action_gestion_comment_id) values ($1,$2)",
-                [$document_id,$this->ag_description_id]);
+            if ( ! empty ($document_saved)) {
+                foreach ($document_saved as $document_id) {
+                    $this->db->exec_sql("insert into action_comment_document(document_id,action_gestion_comment_id) values ($1,$2)",
+                        [$document_id,$this->ag_id]);
+                }
+            }
         }
         $this->insert_operation();
         $this->insert_action();
