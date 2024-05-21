1c60a15414e8c14025d7e3b8b78e49760c98fd18
sparkyx <danydb@noalyss.eu>
2024-05-21 18:32:53 +0200

    unit test

--- unit-test/include/class/operation_exerciceTest.php
+++ unit-test/include/class/operation_exerciceTest.php
@@ -92,7 +92,7 @@ class Operation_ExerciceTest extends TestCase
      *  tearDownAfterClass() template methods is calleafter the last test of the test case class is run,
      *
      */
-    static function atearDownAfterClass(): void
+    static function tearDownAfterClass(): void
     {
         global $g_connection;
         global $aOperation;
