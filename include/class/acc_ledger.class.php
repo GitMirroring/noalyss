ee6a6d15bcf776fa99de36c0fbee2ab79155024f
sparkyx <danydb@noalyss.eu>
2024-07-27 15:07:37 +0200

    typo documentation

--- include/class/acc_ledger.class.php
+++ include/class/acc_ledger.class.php
@@ -580,7 +580,7 @@ class Acc_Ledger  extends jrn_def_sql
      * @brief Show a select list   of the ledgers you can access in
      * writing, reading or simply accessing.
      * @param $p_type = ALL or the type of the ledger (ACH,VEN,FIN,ODS)
-     * @param $p_access =3 for READ and WRITE, 2 for write and 1 for readonly
+     * @param $p_access =3 for READ or WRITE, 2 for write and 1 for readonly
      * @param Boolean TRUE all ledger are selected, or FALSE only enable
      * \return     object HtmlInput select
      * 
