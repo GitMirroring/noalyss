e279559f7d0996b42a581f7d3f213e545bc2836d
sparkyx <danydb@noalyss.eu>
2024-08-19 20:49:41 +0200

    PHPUnit : update test

--- unit-test/include/class/acc_ledgerTest.php
+++ unit-test/include/class/acc_ledgerTest.php
@@ -899,7 +899,7 @@ class Acc_LedgerTest extends TestCase
         ob_end_clean();
         \Noalyss\Facility::save_file(__DIR__."/file", "acc_ledger-input_new.html", $result);
         $size=filesize(__DIR__."/file/acc_ledger-input_new.html");
-        $this->assertTrue($size == 15653  ," output input_new is not what it is expected");
+        $this->assertTrue($size == 15664  ," output input_new is not what it is expected");
 
     }
 
