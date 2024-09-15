62bf9622c3cdd2dda60341ad1bf245708a27559f
sparkyx <danydb@noalyss.eu>
2024-09-15 15:33:30 +0200

    Cosmetic

--- include/tax_summary.inc.php
+++ include/tax_summary.inc.php
@@ -63,6 +63,7 @@ if ( $http->get("do","string","no") == "display")
             echo '<span class="warning">';
             echo $e->getMessage();
             echo '</span>';
+            throw $e;
 
         }
         echo '<ul class="aligned-block">';
