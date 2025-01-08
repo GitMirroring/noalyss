58931e4b7b0d194ab7bf4fc3f0788038ebdc4bec
sparkyx <danydb@noalyss.eu>
2025-01-08 19:59:32 +0100

    Cosmetic : Set color

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,5 +1,4 @@
 -- change color selected menu
-update parm_appearance set a_value='#000074' where a_code='MENU1-SELECTED' and a_value='#7191ea';
 update parm_appearance set a_value='#FAFAFA' where a_code='BODY' and lower(a_value)='#ffffff';
 update parm_appearance set a_value='#FAFAFA' where a_code='FOLDER' and lower(a_value)='#ffffff';
 update parm_appearance set a_value='#506cb8 ' where a_code='MENU1-SELECTED' and lower(a_value)='#000074 ';
