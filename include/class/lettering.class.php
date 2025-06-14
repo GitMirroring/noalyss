37cac047974426ab3453d07f33affd6e072c8b81
sparkyx <danydb@noalyss.eu>
2025-06-14 14:44:52 +0200

    PHP8.3 deprecated dynamic data member

--- include/class/lettering.class.php
+++ include/class/lettering.class.php
@@ -44,6 +44,8 @@
 #[\AllowDynamicProperties]
 class Lettering
 {
+ ///@todo Lettering must be rewritten and use __set and __get
+ /// instead of an array + data_member
 
     protected $variable=array("account"=>"account", /* the accounting of the j_id (use by Lettering_Account) */
         "quick_code"=>"quick_code", /* the quick_code of the j_id (used by Lettering_Card) */
@@ -59,6 +61,10 @@ class Lettering
     var $sql_ledger;
     var $object_type;
     var $content;
+    var $fil_amount_max;
+    var $fil_amount_min;
+    var $fil_deb;
+    var $linked;
 
     /**
      * constructor
