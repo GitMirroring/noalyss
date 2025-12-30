40d94bb815558d065fad8c6f7fd0b61b3bb08eb9
sparkyx <danydb@noalyss.eu>
2025-12-30 15:15:44 +0100

    protect against wrong character

--- include/class/document.class.php
+++ include/class/document.class.php
@@ -190,6 +190,7 @@ class Document
 
             chdir($dirname);
             $filename=($p_filename == "")?$row['md_filename']:$p_filename;
+            $filename= sanitize_filename($filename);
             $exp=$this->db->lo_export($row['md_lob'], $dirname.DIRECTORY_SEPARATOR.$filename);
             if ($exp===false)
             {
