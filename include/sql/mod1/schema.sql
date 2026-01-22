4b2adca0cd49d7cb1aab5468175a03a8f53101c7
sparkyx <danydb@noalyss.eu>
2026-01-22 15:28:01 +0100

    Fix Bug : compatibility postgresql 16

--- include/sql/mod1/schema.sql
+++ include/sql/mod1/schema.sql
@@ -1,13 +1,20 @@
 
 
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
-SET escape_string_warning = off;
+SET row_security = off;
 
 
 CREATE SCHEMA comptaproc;
+
+
 
 CREATE SCHEMA tva_belge;
 
